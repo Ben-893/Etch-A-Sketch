ada953ef7343e2984471529a6cb902a9a5aa7ccb
Ben-893 <ben.smith.gordon@gmail.com>
2020-12-08 23:00:48 +0000

    Added some basic setup for canvas, and a random starting point for the etch-a-sketch board

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,22 @@
 // Select the elements on the page - canvas, shake button
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d');
 
+const shakeButton = document.querySelector('.shake');
 // Setup the canvas for drawing
+const { width, height } = canvas;
 
+let x = Math.floor(Math.random() * width)
+let y = Math.floor(Math.random() * height)
+
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 10;
+
+ctx.beginPath(); // Start the drawing
+ctx.moveTo(x, y);
+ctx.lineTo(x, y);
+ctx.stroke();
 // Write a draw function
 
 // Write a handler for the keys
