a5065ad881da79ffe5ec5c63e8db2469d7b5df02
Ben-893 <ben.smith.gordon@gmail.com>
2020-12-08 22:16:11 +0000

    Created an index template and JS layout

---
+++ etch-a-sketch.js
@@ -0,0 +1,11 @@
+// Select the elements on the page - canvas, shake button
+
+// Setup the canvas for drawing
+
+// Write a draw function
+
+// Write a handler for the keys
+
+// Clear and Shake function
+
+// Listen for Arrow keys
