faf156d9245f8156bdcf48379ace1f7e96ea5ba5
Ben-893 <ben.smith.gordon@gmail.com>
2020-12-09 14:47:31 +0000

    Added the ability to draw with arrow keys

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -6,21 +6,37 @@ const shakeButton = document.querySelector('.shake');
 // Setup the canvas for drawing
 const { width, height } = canvas;
 
-let x = Math.floor(Math.random() * width)
-let y = Math.floor(Math.random() * height)
+let x = Math.floor(Math.random() * width);
+let y = Math.floor(Math.random() * height);
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = 10;
 
-ctx.beginPath(); // Start the drawing
+ctx.beginPath();
 ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 // Write a draw function
+function draw({ key }) {
+  console.log(key);
+  ctx.beginPath();
+  ctx.moveTo(x, y);
 
+  x -= 10;
+  y -= 10;
+
+  ctx.lineTo(x, y);
+  ctx.stroke();
+}
 // Write a handler for the keys
-
+function handleKey(e) {
+  if (e.key.includes('Arrow')) {
+    e.preventDefault();
+    draw({ key: e.key });
+  }
+}
 // Clear and Shake function
 
 // Listen for Arrow keys
+window.addEventListener('keydown', handleKey);
